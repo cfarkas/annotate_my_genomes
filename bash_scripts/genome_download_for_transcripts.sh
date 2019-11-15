bd0dde469c3fad27da1f8abbe4488bda71977e36
cfarkas <carlosfarkas@gmail.com>
2019-11-15 12:22:02 -0300

    Add files via upload

---
+++ bash_scripts/genome_download_for_transcripts.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+genome=${1}
+
+if [ "$1" == "-h" ]; then
+  echo ""
+  echo "Usage: bash ./`basename $0` {genome}"
+  echo ""
+  echo "This script will download and index the specified genome from the uscs server (goldenpath)"
+  echo ""
+  echo "{genome}: Name of the genome assembly"
+  echo ""
+  exit 0
+fi
+
+if [ "$1" == "-help" ]; then
+  echo ""
+  echo "Usage: bash ./`basename $0` {genome}"
+  echo ""
+  echo "This script will download and index the specified genome from the uscs server (goldenpath)"
+  echo ""
+  echo "{genome}: Name of the genome assembly"
+  echo ""
+  exit 0
+fi
+if [ "$1" == "--h" ]; then
+  echo ""
+  echo "Usage: bash ./`basename $0` {genome}"
+  echo ""
+  echo "This script will download and index the specified genome from the uscs server (goldenpath)"
+  echo ""
+  echo "{genome}: Name of the genome assembly"
+  echo ""
+  exit 0
+fi
+
+if [ "$1" == "--help" ]; then
+  echo ""
+  echo "Usage: bash ./`basename $0` {genome}"
+  echo ""
+  echo "This script will download and index the specified genome from the uscs server (goldenpath)"
+  echo ""
+  echo "{genome}: Name of the genome assembly"
+  echo ""
+  exit 0
+fi
+
+[ $# -eq 0 ] && { echo "Usage: bash ./`basename $0` {genome}"; exit 1; }
+
+if [ $# -ne 1 ]; then
+  echo 1>&2 "Usage: bash ./`basename $0`  {genome}"
+  exit 3
+fi
+
+# Obtaining {genome}.fa genome, and indexing
+wget http://hgdownload.cse.ucsc.edu/goldenpath/${genome}/bigZips/${genome}.2bit
+
+if [ -f twoBitToFa ]; then
+    echo "twoBitToFa script found. Continue:"
+    echo ""
+    : 
+else
+    echo "Downloading twoBitToFa script"
+    wget http://hgdownload.cse.ucsc.edu/admin/exe/linux.x86_64/twoBitToFa 
+fi
+
+chmod 755 twoBitToFa
+./twoBitToFa ${genome}.2bit ${genome}.fa
+samtools faidx ${genome}.fa
+echo ""
+echo "All Done. ${genome}.fa is located in the current directory"
