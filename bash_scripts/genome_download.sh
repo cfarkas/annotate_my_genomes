8c411b6c815540fda691e230d781c3b9a764ffde
cfarkas <carlosfarkas@gmail.com>
2020-05-14 22:38:09 -0600

    Update genome_download.sh

--- bash_scripts/genome_download.sh
+++ bash_scripts/genome_download.sh
@@ -8,7 +8,7 @@ if [ "$1" == "-h" ]; then
   echo ""
   echo "This program will download and index the specified genome from the uscs server (goldenpath) including annotation"
   echo ""
-  echo "[genome]: Name of the genome assembly (check here: https://genome.ucsc.edu/cgi-bin/hgGateway)"
+  echo "[genome]: Name of the genome assembly"
   echo ""
   exit 0
 fi
@@ -19,18 +19,17 @@ if [ "$1" == "-help" ]; then
   echo ""
   echo "This program will download and index the specified genome from the uscs server (goldenpath) including annotation"
   echo ""
-  echo "[genome]: Name of the genome assembly (check here: https://genome.ucsc.edu/cgi-bin/hgGateway)"
+  echo "[genome]: Name of the genome assembly"
   echo ""
   exit 0
 fi
-
 if [ "$1" == "--h" ]; then
   echo ""
   echo "Usage: ./`basename $0` [genome]"
   echo ""
   echo "This program will download and index the specified genome from the uscs server (goldenpath) including annotation"
   echo ""
-  echo "[genome]: Name of the genome assembly (check here: https://genome.ucsc.edu/cgi-bin/hgGateway)"
+  echo "[genome]: Name of the genome assembly"
   echo ""
   exit 0
 fi
@@ -41,7 +40,7 @@ if [ "$1" == "--help" ]; then
   echo ""
   echo "This program will download and index the specified genome from the uscs server (goldenpath) including annotation"
   echo ""
-  echo "[genome]: Name of the genome assembly (check here: https://genome.ucsc.edu/cgi-bin/hgGateway)"
+  echo "[genome]: Name of the genome assembly"
   echo ""
   exit 0
 fi
@@ -55,6 +54,7 @@ fi
 
 # Obtaining {genome}.fa genome, and indexing
 wget http://hgdownload.cse.ucsc.edu/goldenpath/${genome}/bigZips/${genome}.2bit
+wget http://hgdownload.soe.ucsc.edu/goldenPath/${genome}/database/refGene.txt.gz
 if [ -f twoBitToFa ]; then
     echo "twoBitToFa script found. Continue:"
     echo ""
@@ -66,5 +66,17 @@ fi
 chmod 755 twoBitToFa
 ./twoBitToFa ${genome}.2bit ${genome}.fa
 samtools faidx ${genome}.fa
+
+if [ -f genePredToGtf ]; then
+    echo "genePredToGtf script found. Continue:"
+    echo ""
+    : 
+else
+    echo "Downloading genePredToGtf script"
+    wget http://hgdownload.cse.ucsc.edu/admin/exe/linux.x86_64/genePredToGtf 
+fi
+chmod 755 genePredToGtf
+gunzip refGene.txt.gz
+cut -f 2- refGene.txt | ./genePredToGtf file stdin -source=${genome}_Ref  ${genome}.gtf
 echo ""
-echo "All done. ${genome}.fa is located in the current directory"
+echo "All done. ${genome}.fa and ${genome}.gtf files are located in the current directory"
