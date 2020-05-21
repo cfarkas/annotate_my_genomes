803be6656c4f71da79e6711522947c1d8fbc9e82
cfarkas <carlosfarkas@gmail.com>
2020-05-20 18:33:42 -0600

    Add files via upload

---
+++ strg_prep.pl
@@ -0,0 +1,36 @@
+#!/bin/env perl
+#Usage: mstrg_prep.pl merged.gtf > merged_prep.gtf
+use strict;
+my %g; # gene_id => \%ref_gene_ids (or gene_names)
+my @prep; # array of [line, original_id]
+while (<>) {
+ s/ +$//;
+ my @t=split(/\t/);
+ unless (@t>8) { print $_; next }
+ my ($gid)=($t[8]=~m/gene_id "(MSTRG\.\d+)"/);
+ if ($gid) {
+   push(@prep, [$_, $gid]);
+   my ($rn)=($t[8]=~m/ref_gene_id "([^"]+)/);
+   #or for gene_name:
+   #my ($rn)=($t[8]=~m/gene_name "([^"]+)/);
+   if ($rn) {
+     my $h=$g{$gid};
+     if ($h) { $h->{$rn}=1 }
+     else { $g{$gid}= { $rn=>1 } }
+   }
+ }
+ else { print $_ }
+}
+my ($prevgid, $gadd);
+foreach my $d (@prep) {
+ my ($line, $gid)=@$d;
+ if ($prevgid ne $gid) {
+    $prevgid=$gid;
+    $gadd='';
+    if (my $gd=$g{$gid}) {
+      $gadd='|'.join('|', (sort(keys(%$gd))))
+    }
+ }
+ $line=~s/gene_id "MSTRG\.\d+/gene_id "$gid$gadd/ if $gadd;
+ print $line;
+}
