1b8bcb1fa5d93c3d579a1794703ecd09002cb5b6
cfarkas <carlosfarkas@gmail.com>
2021-02-07 18:13:44 -0600

    Update makefile.sh

--- makefile.sh
+++ makefile.sh
@@ -1,23 +1,10 @@
 #!/bin/bash
 
+# make
 dir=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
-mkdir swissprot
-cd swissprot
-wget ftp://ftp.ncbi.nlm.nih.gov/blast/db/swissprot.tar.gz
-gunzip swissprot.tar.gz
-tar -xvf swissprot.tar
-SWISSPROT_PATH=$PWD
-echo "$SWISSPROT_PATH"
-cd ..
-cd test
-echo 'SWISSPROT_DB="'$SWISSPROT_PATH'/swissprot"' >> gawn_config.sh
-echo '#' >> gawn_config.sh
-cd ..
 mkdir bin
 mkdir genome_1
 mkdir get_transcripts
-cp ./test/gawn_config.sh ./genome_1/
-cp ./test/gawn_config.sh ./bash_scripts/
 git clone https://github.com/cfarkas/shc.git
 cd shc/
 ./autogen.sh
