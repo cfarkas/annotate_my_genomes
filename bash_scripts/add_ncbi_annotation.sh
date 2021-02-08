1309c847129897ad82009ccc5eb8deab4dfb9038
cfarkas <carlosfarkas@gmail.com>
2021-02-08 12:26:37 -0600

    Update add_ncbi_annotation.sh

--- bash_scripts/add_ncbi_annotation.sh
+++ bash_scripts/add_ncbi_annotation.sh
@@ -328,7 +328,7 @@ rm cds.fa cds.bed prot.fa
 mv cds.fixed.fa cds.fa
 mv cds.fixed.bed cds.bed 
 mv prot.fixed.fa prot.fa
-rm merged.fixed.coding.gtf namelist
+rm merged.fixed.coding.gtf namelist coding-transcripts.fa coding-genes.gtf merged.fixed.lncRNAs.gtf other-genes.gtf
 echo ""
 ###############################
 # Configuring Summary Results #
