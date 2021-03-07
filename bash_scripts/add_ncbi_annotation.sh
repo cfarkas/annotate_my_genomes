2fd16cf0c27c0169fe3f15b6c4c76f0ada8fbeb6
cfarkas <carlosfarkas@gmail.com>
2021-03-07 09:18:17 -0600

    Update add_ncbi_annotation.sh

--- bash_scripts/add_ncbi_annotation.sh
+++ bash_scripts/add_ncbi_annotation.sh
@@ -97,7 +97,6 @@ rm A A.1 A.2 B B.1 B.2
 ###############################
 awk '{print $1}' namelist > fileA
 awk '{print $2}' namelist > fileB
-#paste -d : fileA fileB | sed 's/\([^:]*\):\([^:]*\)/s%\1%\2%/' > sed.script
 paste -d % fileA fileB > sed.script
 sed -i -e 's/^/s%/' sed.script
 sed -i -e 's/$/%/' sed.script
