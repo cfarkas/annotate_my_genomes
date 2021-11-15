c57ce546e1bac2fe54b23990ece1448c715b580a
cfarkas <carlosfarkas@gmail.com>
2021-11-15 16:46:31 -0300

    adding genome-download-macOSX binary
    adding genome-download-macOSX binary

--- makefile.sh
+++ makefile.sh
@@ -30,12 +30,15 @@ echo "make done. Continue with install"
 ./shc/src/shc -f ./bash_scripts/annotate_my_genomes.sh -o ./annotate-my-genomes
 ./shc/src/shc -f ./bash_scripts/get_transcripts.sh -o ./get-transcripts
 ./shc/src/shc -f ./bash_scripts/genome_download.sh -o ./genome-download
+./shc/src/shc -f ./bash_scripts/genome_download_macOSX.sh -o ./genome-download-macOSX
 ./shc/src/shc -f ./bash_scripts/add_ncbi_annotation.sh -o ./add-ncbi-annotation
-mv annotate-my-genomes get-transcripts genome-download add-ncbi-annotation ./bin/
+mv annotate-my-genomes get-transcripts genome-download genome-download-macOSX add-ncbi-annotation ./bin/
 cp ./bin/annotate-my-genomes ./test/
 cp ./bin/annotate-my-genomes ./genome_1/
 cp ./bin/genome-download ./test/
 cp ./bin/genome-download ./genome_1/
+cp ./bin/genome-download-macOSX ./test/
+cp ./bin/genome-download-macOSX ./genome_1/
 cp ./bin/add-ncbi-annotation ./test/
 cp ./bin/add-ncbi-annotation ./genome_1/
 cp ./bin/get-transcripts ./get_transcripts/
