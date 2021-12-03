86c186a7a1da90e5a70aae2537eba161db2b6f60
cfarkas <carlosfarkas@gmail.com>
2021-12-03 00:31:57 -0300

    Add files via upload

---
+++ additional_scripts/gffcompare_parser.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+### Libraries
+import sys
+import argparse
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    OKRED = '\033[91m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+parser = argparse.ArgumentParser(description="This script will parse gffcompare tmap file, including ncbiRefSeqLink.txt file to produce an annotated transcript csv file.")
+parser.add_argument('--tmap_file', help="path to gffcompare tmap file. In example: UCSC_compare.stringtie_chr33.gtf.tmap or NCBI_compare.stringtie_chr33.gtf.tmap", type=str)
+parser.add_argument('--genome_name', help="Genome name in UCSC notation. In example: mm10, galGal6, hg38, rn6", type=str)
+args = parser.parse_args()
+
+name = sys.argv[0]
+tmap_file = str(sys.argv[2])
+genome_name = str(sys.argv[4])
+
+print(bcolors.OKGREEN + "Command line:", str(sys.argv) + bcolors.ENDC)
+print("")
+print(bcolors.OKGREEN + "--- Loading python libraries --" + bcolors.ENDC)
+print("")
+import pandas as pd
+
+df = pd.read_csv(tmap_file, sep = '\t')
+print("Total number of transcripts:", df.shape[0])
+print("")
+
+df2 = df[~df.ref_id.astype(str).str.contains('-')]
+novel_transcripts = df[df.ref_id.astype(str).str.contains('-')]
+print("Number of annotated transcripts:", df2.shape[0])
+print("Number of novel transcripts:", novel_transcripts.shape[0])
+print("")
+
+df3 = df2[["ref_gene_id", "ref_id", "class_code", "qry_gene_id", "qry_id", "num_exons", "FPKM", "TPM"]]
+df_novel_transcripts = novel_transcripts[["ref_gene_id", "ref_id", "class_code", "qry_gene_id", "qry_id", "num_exons", "FPKM", "TPM"]]
+
+df3.head(10)
+df_novel_transcripts.head(10)
+
+colnames=['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18']
+dfA1 = pd.read_csv('ncbiRefSeqLink.txt', sep = '\t', low_memory=False, names=colnames, header=None)
+dfA1.head(10)
+
+dfA2 = dfA1[['0', '1', '2', '3', '5', '14', '16']]
+dfA2.head(10)
+
+dfA2 = dfA2.rename(columns={'0': 'ref_id', '1': 'Annotation Status', '2' : 'NCBI RefSeq Gene ID', '3' : 'Transcript Description', '5' : 'NCBI RefSeq Protein ID', '14' : 'Alternative Gene Name', '16' : 'RefSeq Transcript Info'})
+dfA2.sample(10)
+
+result = pd.merge(df3, dfA2, on='ref_id', how='inner')
+result.sample(10)
+print("Number of Joined Transcripts:", result.shape[0])
+print("")
+result.to_csv('Transcript_Annotation.csv', index=False)
+novel_transcripts.to_csv('Novel_Transcripts.csv', index=False)
+print(bcolors.OKGREEN + "Transcript_Annotation.csv and Novel_Transcripts.csv were succesfully produced"+ bcolors.ENDC)
+print("")
+print(bcolors.OKGREEN + "::: All Done :::" + bcolors.ENDC)
+print("")
+
+
+
