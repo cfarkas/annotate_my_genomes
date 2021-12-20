4b89a8b8f16384399fca350cf6a789a565914ffe
cfarkas <carlosfarkas@gmail.com>
2021-12-20 11:47:57 -0300

    Update gawn_config.sh
    skip genome indexing

--- test/gawn_config.sh
+++ test/gawn_config.sh
@@ -8,7 +8,7 @@
 NCPUS=10                    # Number of CPUs to use for analyses (int, 1+)
 
 # Genome indexing
-SKIP_GENOME_INDEXING=0      # 1 to skip genome indexing, 0 to index it
+SKIP_GENOME_INDEXING=1      # 1 to skip genome indexing, 0 to index it
 
 # Genome annotation with transcriptome
 # NOTE: do not use compressed fasta files
