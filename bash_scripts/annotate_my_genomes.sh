f922ab9e5c892cc305f81d53e5491745a4e3ea72
cfarkas <carlosfarkas@gmail.com>
2022-04-28 10:24:18 -0400

    Update annotate_my_genomes.sh

--- bash_scripts/annotate_my_genomes.sh
+++ bash_scripts/annotate_my_genomes.sh
@@ -243,6 +243,7 @@ sed -i -e 's/$/%/' sed.script
 cat ${a_DIR}/${stringtie_input} | parallel --pipe -j ${t} sed -f sed.script > final_annotated.gtf
 rm -f fileA fileB *tmap.1 *tmap.2
 # sorting GTF file
+rm -r -f gff3sort
 git clone https://github.com/cfarkas/gff3sort.git
 perl ./gff3sort/gff3sort.pl final_annotated.gtf > final_annotated.sorted.gtf
 rm final_annotated.gtf
@@ -251,6 +252,7 @@ printf "${PURPLE}::: Done. Gene_id field was replaced in the StringTie.gtf file 
 echo ""
 printf "${PURPLE}::: Moving gffcompare results to gffcompare_outputs folder ...\n"
 echo ""
+rm -r -f gffcompare_outputs_UCSC
 mkdir gffcompare_outputs_UCSC
 mv *.loci *.stats *.refmap *.tmap *.tracking ./gffcompare_outputs_UCSC
 echo ""
@@ -276,6 +278,7 @@ printf "${YELLOW}::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::${C
 echo ""
 printf "${PURPLE}::: Downloading GAWN annotation folder. See https://github.com/enormandeau/gawn.git${CYAN}\n"
 echo ""
+rm -r -f gawn
 git clone https://github.com/cfarkas/gawn.git
 cd gawn/02_infos/
 dir2=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
@@ -477,9 +480,11 @@ printf "${YELLOW}:::::::::::::::::::::::::::::::::::::::::::::::::::::${CYAN}\n"
 
 echo ""
 printf "${PURPLE}::: Moving results to the specified directory :::${CYAN}\n"
+rm -r -f output_files
 mkdir output_files
 mv candidate_lncRNA_classes.txt final_annotated.gtf final_annotated.gff transcripts.fa cds.fa prot.fa coding_transcripts.gtf Stats.txt transcriptome.swissprot novel-cds.fa novel-prot.fa ./output_files
 rm -r -f *feelncfilter.log genes.gtf pipeliner* NM_coding.gtf candidate_lncRNA.gtf* coding-transcripts.fa.transdecoder_dir.__* transcripts.fa.fai
+rm -r -f transdecoder
 mkdir transdecoder
 mv coding-transcripts.fa.transdecoder.* ./transdecoder
 mv UCSC_compare.annotated.gtf ./gffcompare_outputs_UCSC
