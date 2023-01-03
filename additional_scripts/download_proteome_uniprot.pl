f38d5da051ab73dc127bc7cb163799948d2cc6e1
cfarkas <carlosfarkas@gmail.com>
2023-01-03 17:44:56 -0300

    Update download_proteome_uniprot.pl
    2023 update from here: https://www.uniprot.org/help/api_downloading

--- additional_scripts/download_proteome_uniprot.pl
+++ additional_scripts/download_proteome_uniprot.pl
@@ -5,7 +5,8 @@ use HTTP::Date;
 
 my $taxon = $ARGV[0]; # Taxonomy identifier of organism.
 
-my $query = "https://www.uniprot.org/uniprot/?query=organism:$taxon&format=fasta";
+my $query = "https://rest.uniprot.org/uniprotkb/search?query=organism_id:$taxon&format=fasta";
+    
 my $file = $taxon . '.fasta';
 
 my $contact = ''; # Please set a contact email address here to help us debug in case of problems (see https://www.uniprot.org/help/privacy).
@@ -15,13 +16,13 @@ my $response = $agent->mirror($query, $file);
 if ($response->is_success) {
   my $results = $response->header('X-Total-Results');
   my $release = $response->header('X-UniProt-Release');
-  my $date = sprintf("%4d-%02d-%02d", HTTP::Date::parse_date($response->header('Last-Modified')));
+  my $date = sprintf("%4d-%02d-%02d", HTTP::Date::parse_date($response->header('X-UniProt-Release-Date')));
   print "Downloaded $results entries of UniProt release $release ($date) to file $file\n";
-}
-elsif ($response->code == HTTP::Status::RC_NOT_MODIFIED) {
-  print "Data for taxon $taxon is up-to-date.\n";
-}
-else {
-  die 'Failed, got ' . $response->status_line .
-    ' for ' . $response->request->uri . "\n";
-}
+  }
+  elsif ($response->code == HTTP::Status::RC_NOT_MODIFIED) {
+    print "Data for taxon $taxon is up-to-date.\n";
+  }
+  else {
+    die 'Failed, got ' . $response->status_line .
+      ' for ' . $response->request->uri . "\n";
+  }
