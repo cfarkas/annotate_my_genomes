34c6b6372ec163d2b388901b8648fddb1b25ae43
cfarkas <carlosfarkas@gmail.com>
2023-01-04 10:08:26 -0300

    Update download_proteome_uniprot.pl

--- additional_scripts/download_proteome_uniprot.pl
+++ additional_scripts/download_proteome_uniprot.pl
@@ -1,42 +1,50 @@
 #!/usr/bin/perl
 
-use strict;
 use warnings;
+use strict;
 use LWP::UserAgent;
 use HTTP::Date;
-use HTTP::Status;  # Add "use" statement for HTTP::Status module
 
-# Print usage statement
-if (@ARGV != 1) {
-  print STDERR "Usage: $0 TAXON_ID\n";
-  print STDERR "Downloads FASTA records for the specified taxonomy ID from UniProt.\n";
-  exit 1;  # Exit with non-zero status code to indicate error
+# Check that a taxonomy identifier was passed as a command line argument
+if (!$ARGV[0]) {
+  die "Error: No taxonomy identifier specified.\nUsage: perl download_proteome_uniprot.pl <taxonomy_id>\n";
 }
 
-my $taxon = $ARGV[0]; # Taxonomy identifier of organism.
+# Taxonomy identifier of top node for query, e.g. 2 for Bacteria, 2157 for Archaea, etc.
+# (see https://www.uniprot.org/taxonomy)
+my $top_node = $ARGV[0];
 
-my $query = "https://rest.uniprot.org/uniprotkb/search?query=organism_id:$taxon&format=fasta";
+# Create a user agent for making HTTP requests
+my $agent = LWP::UserAgent->new;
 
-my $file = $taxon . '.fasta';
+# Get a list of all reference proteomes of organisms below the given taxonomy node.
+my $query_list = "https://rest.uniprot.org/proteomes/stream?query=reference:true+taxonomy_id:$top_node&format=list";
 
-my $contact = ''; # Please set a contact email address here to help us debug in case of problems (see https://www.uniprot.org/help/privacy).
-my $agent = LWP::UserAgent->new(agent => "libwww-perl $contact");
-my $response = $agent->mirror($query, $file);
+my $response_list = $agent->get($query_list);
 
-if ($response->is_success) {
-  # Print number of results and release info
-  my $results = $response->header('X-Total-Results');
-  my $release = $response->header('X-UniProt-Release');
-  my $date = sprintf("%4d-%02d-%02d", HTTP::Date::parse_date($response->header('X-UniProt-Release-Date')));
-  print "Downloaded $results entries of UniProt release $release ($date) to file $file\n";
+# Check for HTTP errors
+if (!$response_list->is_success) {
+  die 'Failed to get proteome list: ' . $response_list->status_line .
+    ' for ' . $response_list->request->uri . "\n";
 }
-elsif ($response->code == HTTP::Status::RC_NOT_MODIFIED) {
-  # Print message if data is already up-to-date
-  print "Data for taxon $taxon is up-to-date.\n";
-}
-else {
-  # Print error message and exit with non-zero status code
-  warn 'Failed, got ' . $response->status_line .
-    ' for ' . $response->request->uri . "\n";
-  exit 1;
+
+# For each proteome, mirror its set of UniProt entries in compressed FASTA format.
+for my $proteome (split(/\n/, $response_list->content)) {
+  my $file = $proteome . '.fasta.gz';
+  my $query_proteome = "https://rest.uniprot.org/uniprotkb/stream?query=proteome:$proteome&format=fasta&compressed=true";
+  my $response_proteome = $agent->mirror($query_proteome, $file);
+
+  # Check for HTTP errors
+  if ($response_proteome->is_success) {
+    my $release = $response_proteome->header('x-uniprot-release');
+    my $date = $response_proteome->header('x-uniprot-release-date');
+    print "File $file: downloaded entries of UniProt release $release ($date)\n";
+  }
+  elsif ($response_proteome->code == HTTP::Status::RC_NOT_MODIFIED) {
+    print "File $file: up-to-date\n";
+  }
+  else {
+    die 'Failed to download proteome: ' . $response_proteome->status_line .
+      ' for ' . $response_proteome->request->uri . "\n";
+  }
 }
